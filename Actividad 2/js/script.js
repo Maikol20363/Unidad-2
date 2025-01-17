ed2527de1b74bf259b77356d92591e31cd9bbb4a
Maikol20363 <jrojsib2309@g.educaand.es>
2025-01-17 11:56:50 +0100

    Add files via upload

---
+++ Actividad 2/js/script.js
@@ -0,0 +1,13 @@
+function mostrarAlerta() {
+    alert("¡Has pulsado el boton!");
+    segundaFuncion();
+}
+
+function segundaAlerta() {
+    alert("Has pulsado el boton!");
+    segundaFuncion();
+}
+
+funcion segundaFuncion() {
+    alert("¿A que si?")
+}
