fa565cb2b332054dd66abe32ac6d1637cc6cb790
Maikol20363 <jrojsib2309@g.educaand.es>
2025-01-23 14:26:13 +0100

    Update script.js

--- Actividad 2/js/script.js
+++ Actividad 2/js/script.js
@@ -1,13 +1,13 @@
-function mostrarAlerta() {
-    alert("¡Has pulsado el boton!");
-    segundaFuncion();
-}
-
-function segundaAlerta() {
-    alert("Has pulsado el boton!");
-    segundaFuncion();
-}
-
-funcion segundaFuncion() {
-    alert("¿A que si?")
-}
+        function saludar() {
+            alert("Hola");
+            segundaFuncion();
+        }
+        
+        function despedirse() {
+            alert(";-)");
+            segundaFuncion();
+        }
+        
+        funcion emoji() {
+            alert("¿A que si?")
+        }
