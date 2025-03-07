12feed7e62ecac8f55d4a7485a09ad05bed2c23d
Maikol20363 <jrojsib2309@g.educaand.es>
2025-03-07 23:43:09 +0100

    Add files via upload

---
+++ Actividad 4/js(4)/script.js
@@ -0,0 +1,5 @@
+function Calcular() {
+    let a = parseInt(promt("dime el primer numero"),10);
+    let b = parseInt(prompt("dime el segundo numero"),10);
+    alert ("El primer numero es:" + a + "\nEl segundo numero es: " + b + "\nLa suma es: " + (a+b) +"\nLa resta es " + (a-b) + "\nLa multiplicacion es" + (a*b) + "\nLa division es " + (a/b) + "\nel resto es " + (a%b) +"")
+}
